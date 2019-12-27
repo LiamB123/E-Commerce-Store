62e2f9346175faf7bb26d828f18d43b6315df2b4
Ubuntu <ubuntu@ip-172-31-34-60.eu-west-1.compute.internal>
2019-12-27 17:41:47 +0000

    updated products app to work with Postgres

--- products/models.py
+++ products/models.py
@@ -6,11 +6,14 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name=models.CharField(max_length=200, default='')
-    description= models.TextField()
-    price=models.DecimalField(max_digits=6, decimal_places=2)
-    image=models.ImageField(upload_to='media/img')
+    name=models.CharField(max_length=100, default='')
     team=models.CharField(max_length=50, default='')
+    description= models.TextField(max_length=200)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+    image=models.ImageField(upload_to='img')
+    
     
     def __str__(self):
         return self.name
+        
+       
