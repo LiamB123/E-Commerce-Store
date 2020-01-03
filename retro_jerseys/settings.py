68cca62ac91d6062b5491c2d054025ff88edaf8b
Ubuntu <ubuntu@ip-172-31-34-60.eu-west-1.compute.internal>
2020-01-03 18:36:16 +0000

    changed env vaiables to heroku

--- retro_jerseys/settings.py
+++ retro_jerseys/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
-import env
+#import env
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
