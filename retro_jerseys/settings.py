2e75ab85d33242202f509b29c113dbe70b473e84
Ubuntu <ubuntu@ip-172-31-34-60.eu-west-1.compute.internal>
2020-01-07 18:08:14 +0000

    Debug turned off

--- retro_jerseys/settings.py
+++ retro_jerseys/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 
 
